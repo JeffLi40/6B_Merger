41078bab7a9a2c99619d5a7724cc90c4b90576fe
Jeff Li <jli40@stuy.edu>
2019-05-13 20:44:30 -0400

    did hw

--- SubSortThenMerge_Sorter.java
+++ SubSortThenMerge_Sorter.java
@@ -8,11 +8,15 @@ public class SubSortThenMerge_Sorter {
         sortRecursive( 0, sortThis.size());
     }
     private void sortRecursive( int inclusiveStart, int exclusiveEnd) {
-        //if( exclusiveEnd - inclusiveStart == 1) do nothing //decision between base case and recursive case
+        //if( exclusiveEnd - inclusiveStart == 1) instructions for base case: do nothing //decision between base case and recursive case
         if( exclusiveEnd - inclusiveStart != 1) {
+          //instructions for recursive case
+          //additional processing: calculate the midway index
           int midway = (exclusiveEnd + inclusiveStart + 1) / 2;
+          //invocation of recursive abstractionL when I am asked to sort a sublist indexed i to j, I can sort half of that sublist.
           sortRecursive( inclusiveStart, midway);
           sortRecursive( midway, exclusiveEnd);
+          //combine: merge the two sorted sublists
           sortThis.merge( inclusiveStart, midway, exclusiveEnd);
         }
     }
